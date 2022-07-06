ece55421a66abd238ec3e92b685bfb85d3c69d2e
ang94rocha <86586748+ang94rocha@users.noreply.github.com>
2022-07-06 09:21:15 -0700

    Add files via upload

---
+++ week4challenges.js
@@ -0,0 +1,7 @@
+//Very Easy 
+function min (p1, p2){
+    return Math.min(p1, p2);
+}
+var num1 = Number(prompt("Enter a number: "));
+var num2 = Number(prompt("Enter another number: "));
+console.log("The smaller of the two numbers is ${min(num1,num2)}");
